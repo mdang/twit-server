33d924b7683286cd48aad44632c6e2bf19059e00
Mike Dang <mike.dang@generalassemb.ly>
2017-11-07 17:03:36 -0500

    Working socket.io demo with Twit

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const ejsLayouts = require('express-ejs-layouts');
+const Twit = require('twit');
 const app = express();
+
+const terms = ['javascript', 'coda', 'capitalone'];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -12,7 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(ejsLayouts);
 
-app.use('/', require('./controllers/index'));
+app.get('/', (req, res) => {
+  res.render('index', {
+    terms: terms
+  });
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -32,9 +39,34 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
+const twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+
+server.listen(port, () => {
   console.log('Server started on port %s', port);
+  const io = require('socket.io')(server);
+  
+  io.on('connect', socket => {
+    const stream = twitter.stream('statuses/filter', { track: terms });
+
+    stream.on('tweet', tweet => {
+      const data = {};
+
+      data.name = tweet.user.name;
+      data.screen_name = tweet.user.screen_name;
+      data.user_profile_image = tweet.user.profile_image_url;
+      data.text = tweet.text;
+
+      socket.emit('tweets', data);
+    });
+  });
 });
 
 module.exports = server;
